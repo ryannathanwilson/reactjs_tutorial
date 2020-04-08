e1b419dfe5c3166e3751f994e4e9908fb4c791b2
ryannathanwilson <ryan.nathan.wilson@gmail.com>
2020-04-08 11:23:51 -0400

    first function, element, and rendering with template literals

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function formatName(user) {
+  return `${user.firstName} ${user.lastName}`;
+}
+
+const user = {
+  firstName: 'Ryan',
+  lastName: 'Wilson'
+};
+
+const element = (
+  <div>
+    <h1>Hello, {formatName(user)}</h1>
+    <h2>Good to see you here.</h2>
+  </div>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

e4fdbfbab987bf4fc8b000ccb3c5c0bed79b7d97
ryannathanwilson <ryan.nathan.wilson@gmail.com>
2020-04-10 10:43:34 -0400

    props and components

--- src/index.js
+++ src/index.js
@@ -3,24 +3,60 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function formatName(user) {
-  return `${user.firstName} ${user.lastName}`;
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-const user = {
-  firstName: 'Ryan',
-  lastName: 'Wilson'
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
 };
 
-const element = (
-  <div>
-    <h1>Hello, {formatName(user)}</h1>
-    <h2>Good to see you here.</h2>
-  </div>
-)
-
 ReactDOM.render(
-  element,
+  <Comment 
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
   document.getElementById('root')
 );
 

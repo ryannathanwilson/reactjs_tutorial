fa7c3f26e9573be95400a40cdc539f448bbcdb45
ryannathanwilson <ryan.nathan.wilson@gmail.com>
2020-04-10 12:34:15 -0400

    this binding/events. V confusing.

--- src/index.js
+++ src/index.js
@@ -3,41 +3,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-class Clock extends React.Component{
+class Toggle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
   }
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-  
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
     );
   }
 }
 
 ReactDOM.render(
-  <Clock />,
+  <Toggle />,
   document.getElementById('root')
 );

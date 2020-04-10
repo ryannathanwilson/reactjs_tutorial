c5121635a2c03cce0a1e148a0ec9f8bdef55a985
ryannathanwilson <ryan.nathan.wilson@gmail.com>
2020-04-10 12:08:42 -0400

    states

--- src/index.js
+++ src/index.js
@@ -3,60 +3,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
 
+class Clock extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
 
-function Avatar(props) {
-  return (
-    <img className="Avatar"
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-    />
-  );
-}
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
 
-function UserInfo(props) {
-  return (
-    <div className="UserInfo">
-      <Avatar user={props.user} />
-      <div className="UserInfo-name">
-        {props.user.name}
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <UserInfo user={props.author} />
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'https://placekitten.com/g/64/64',
-  },
-};
 
 ReactDOM.render(
-  <Comment 
-    date={comment.date}
-    text={comment.text}
-    author={comment.author}
-  />,
+  <Clock />,
   document.getElementById('root')
 );
-
